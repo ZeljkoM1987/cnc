623d30df5906157dc3c5a376f8d67e3230013cd7
Zoran Dasic <zorandsc@yahoo.com>
2024-08-30 15:06:50 +0200

    first nazor commit

--- src/app/components/hero/Hero.jsx
+++ src/app/components/hero/Hero.jsx
@@ -1,19 +1,17 @@
 "use client";
 
 import React, { useState } from "react";
-import styles from "./Hero.module.css"
-
-
+import styles from "./Hero.module.css";
 
 export const Hero = () => {
-
-
-    return (
-        <div className={styles.imagewrap}>
-            <img src="/images/hero3.jpg" />
-            <h2 className={styles.text1}>POUZDANOST.</h2>
+  return (
+    <div className={styles.imagewrap}>
+      <img src="/images/hero3.jpg" />
+      
+      <h2 className={styles.text1}>POUZDANOST.</h2>
             <h2 className={styles.text2}>PRECIZNOST.</h2>
             <h2 className={styles.text3}>KVALITET.</h2>
-        </div>
-    );
+     
+    </div>
+  );
 };
