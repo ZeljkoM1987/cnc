4bb38c227a28741d1881ed73f87bb00e32977b88
Željko Milešević <Zeljko.Milesevic@in.mtel.ba>
2024-09-02 10:23:25 +0200

    sec commit

--- src/app/components/hero/Hero.jsx
+++ src/app/components/hero/Hero.jsx
@@ -6,12 +6,13 @@ import styles from "./Hero.module.css";
 export const Hero = () => {
   return (
     <div className={styles.imagewrap}>
-      <img src="/images/hero3.jpg" />
-      
+      <img src="/images/hero4.jpg" />
+   <div className={styles.text}> 
+
       <h2 className={styles.text1}>POUZDANOST.</h2>
             <h2 className={styles.text2}>PRECIZNOST.</h2>
             <h2 className={styles.text3}>KVALITET.</h2>
-     
+    </div>
     </div>
   );
 };
